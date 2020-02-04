f9313087e9d94383d919cdbb10edeb8f3e6ab2c3
Colin Van Oort <omega1563@gmail.com>
2020-02-04 12:59:37 -0500

    Added a new dataset containing county area and population data, allowing for population density calculations. Merged the data in the load_data function.

--- county_data_eda.py
+++ county_data_eda.py
@@ -11,12 +11,12 @@ from pandas_summary import DataFrameSummary
 def main():
     df = load_and_process_data()
 
-    for col in df.columns:
-        print(df[col].describe())
+    # for col in df.columns:
+    #     print(df[col].describe())
 
-    dfs = DataFrameSummary(df)
-    print(dfs.columns_types)
-    print(dfs.columns_stats)
+    # dfs = DataFrameSummary(df)
+    # print(dfs.columns_types)
+    # print(dfs.columns_stats)
 
     pair_plot(df)
 
@@ -34,11 +34,24 @@ def load_and_process_data():
     df.dropna(inplace=True)
     df.sort_values('rank', inplace=True)
     df['id'] = df['id'].str.zfill(5)
-    return df
+    df.rename({'id': 'FIPS'}, axis=1, inplace=True)
+    print(df.columns)
+
+    df2 = pd.read_csv('centroids.csv', na_values=['-'])
+
+    # Clean up and type cast a few columns
+    for col in ['Population (2010)', 'Land Area (km)', 'Land Area(mi)', 'Water Area (km)', 'Water Area (mi)', 'Total Area (km)', 'Total Area (mi)']:
+        df2[col] = df2[col].str.replace(',', '').astype(float)
+
+    df2['FIPS'] = df2['FIPS'].astype(str).str.zfill(5)
+    df2.drop('County', axis=1, inplace=True)
+
+    df3 = pd.merge(df, df2, on='FIPS', how='left', left_index=False, right_index=False)
+    return df3
 
 
 def pair_plot(df):
-    sns.pairplot(df.drop(['id', 'County'], axis=1), plot_kws={'facecolor': 'None', 'edgecolor': sns.xkcd_rgb["denim blue"]})
+    sns.pairplot(df.drop(['FIPS', 'County'], axis=1), plot_kws={'facecolor': 'None', 'edgecolor': sns.xkcd_rgb["denim blue"]})
     plt.tight_layout()
     plt.savefig('county_data_pair.png')
     plt.close()
@@ -49,7 +62,7 @@ def make_upshot_figure(df):
     Replicates the figure from:
         https://www.nytimes.com/2014/06/26/upshot/where-are-the-hardest-places-to-live-in-the-us.html
     """
-    discrete_rank = pd.qcut(df['rank'], 7, labels=False).astype(str)
+    df['Quality of Life'] = pd.qcut(df['rank'], 7, labels=False).astype(str)
 
     with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
         counties = json.load(response)
@@ -57,8 +70,8 @@ def make_upshot_figure(df):
     fig = px.choropleth(
         df,
         geojson=counties,
-        locations='id',
-        color=discrete_rank,
+        locations='FIPS',
+        color='Quality of Life',
         hover_name='County',
         hover_data=['rank', 'income', 'education', 'unemployment', 'disability', 'life', 'obesity'],
         color_discrete_map={
@@ -104,14 +117,14 @@ def make_modified_upshot_figure(df):
     fig = px.choropleth(
         df,
         geojson=counties,
-        locations='id',
+        locations='FIPS',
         color='rank',
         hover_name='County',
         hover_data=['rank', 'income', 'education', 'unemployment', 'disability', 'life', 'obesity'],
         color_continuous_scale='Greens',
-        range_color=[-500, df['rank'].max()],
+        range_color=[-1000, df['rank'].max()],
         labels={
-            'rank': 'Quality of Living Rank',
+            'rank': 'Quality of Life Rank',
             'income': 'Median Income',
             'education': 'College Education',
             'unemployment': 'Unemployment',
